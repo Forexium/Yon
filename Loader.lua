d9d05bb7a31705846486d2a727e346e16e27d636
Forexium <150202229+Forexium@users.noreply.github.com>
2024-07-22 18:41:48 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,14 +1,14 @@
--- >> Script Key >>
 getgenv().script_key = getgenv()['script_key']
--- >> Games >>
+
 local GamesTable = {
-	[3150475059] = "https://api.luarmor.net/files/v3/loaders/f04b37a220b0a222f9d6ac27395e4a05.lua"
+	[3150475059] = "https://api.luarmor.net/files/v3/loaders/749ad0388df8fd981d979d9dbefda694.lua",
+    [1008451066] = "https://api.luarmor.net/files/v3/loaders/7d0911cf869a1a320d7cdfead79aa59c.lua",
 }
--- >> Variables >>
+
 local Link = GamesTable[game.GameId]
--- >> Checks >>
+
 if not Link then
     print("Invalid.")
 end
--- >> Loadstring >>
+
 loadstring(game:HttpGet(Link))()
