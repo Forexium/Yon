2c1ee7bcaff75f18f4946f717760b294ac554f2a
Forexium <150202229+Forexium@users.noreply.github.com>
2024-07-22 18:42:30 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,8 @@
 getgenv().script_key = getgenv()['script_key']
 
 local GamesTable = {
-	[3150475059] = "https://api.luarmor.net/files/v3/loaders/749ad0388df8fd981d979d9dbefda694.lua",
-    [1008451066] = "https://api.luarmor.net/files/v3/loaders/7d0911cf869a1a320d7cdfead79aa59c.lua",
+    [3150475059] = "https://api.luarmor.net/files/v3/loaders/749ad0388df8fd981d979d9dbefda694.lua",
+    [1008451066] = "https://api.luarmor.net/files/v3/loaders/7d0911cf869a1a320d7cdfead79aa59c.lua"
 }
 
 local Link = GamesTable[game.GameId]
